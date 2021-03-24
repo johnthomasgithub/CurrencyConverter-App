23e24e4c35e58d36c3079bbeefce0fda925a5f53
john <johnthomas222000@gmail.com>
2021-03-24 14:08:49 +0530

    CurrencyConverter

---
+++ currency/pro/lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:pro/services/api_client.dart';
+import 'package:pro/widgets/drop_down.dart';
+
+void main() => runApp(new MaterialApp(
+      title: "Currency Converter",
+      home: CurrencyConverter(),
+    ));
+
+class CurrencyConverter extends StatefulWidget {
+  @override
+  _CurrencyConverterState createState() => _CurrencyConverterState();
+}
+
+class _CurrencyConverterState extends State<CurrencyConverter> {
+  final fromController = new TextEditingController();
+  ApiClient client = new ApiClient();
+  Color mainColor = Color(0xFF212936);
+  Color secondColor = Color(0xFF2849E5);
+
+  List<String> currencies;
+  String from;
+  String to;
+  double rate;
+  String result = "";
+
+  Future<List<String>> getCurrrencyList() async {
+    return await client.getCurrencies();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    (() async {
+      List<String> list = await client.getCurrencies();
+      setState(() {
+        currencies = list;
+      });
+    })();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: mainColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 18.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                width: 200.0,
+                child: Text(
+                  "Currency Converter",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      TextField(
+                        onSubmitted: (value) async{
+                          rate = await client.getRate(from, to);
+                          setState(() {
+                            result = (rate * double.parse(value)).toStringAsFixed(3);                          
+                              });
+                        },
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            labelText: "Input Value To Convert",
+                            labelStyle: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontSize: 18,
+                              color: secondColor,
+                            )),
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          customDropDown(currencies, from, (val) {
+                            setState(() {
+                              from = val;
+                            });
+                          }),
+                          FloatingActionButton(
+                            onPressed: () {
+                              String temp=from;
+                              setState(() {
+                                  from = to;
+                                  to=temp;                           
+                                });
+                            },
+                            child: Icon(Icons.swap_horiz),
+                            elevation: 0.0,
+                            backgroundColor: secondColor,
+                          ),
+                          customDropDown(currencies, to, (val) {
+                            setState(() {
+                              to = val;
+                            });
+                          }),
+                        ],
+                      ),
+                      SizedBox(height: 50.0,),
+                      Container(
+                        width: double.infinity,
+                        padding: EdgeInsets.all(16.0),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(8.0),),
+                      child: Column(
+                        children: [
+                          Text("Result",style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                          )),
+                          Text(result,style: TextStyle(
+                            color: secondColor,
+                            fontSize: 36.0,
+                            fontWeight: FontWeight.bold,
+                          ),),
+                        ],
+                      ),),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ currency/pro/lib/services/api_client.dart
@@ -0,0 +1,42 @@
+ import 'dart:convert';
+
+import 'package:flutter/material.dart';
+ import 'package:http/http.dart' as http;
+
+
+
+ class ApiClient{
+   final  Uri currencyURL=Uri.https("free.currconv.com","/api/v7/currencies",{"apiKey":"f6b50f6c2b98d4534537"});
+   
+
+  Future<List<String>> getCurrencies() async {
+    http.Response res=await http.get(currencyURL);
+    if(res.statusCode == 200){
+      var body=jsonDecode(res.body);
+      var list =body["results"];
+      List<String> currencies = (list.keys).toList();
+      //print(currencies);
+      return currencies;
+    }else{
+      throw Exception("Failed to connect to API");
+    }
+
+  }
+  Future<double> getRate(String from,String to) async {
+    final Uri rateUrl = Uri.https('free.currconv.com','/api/v7/convert',{
+     "apiKey":"f6b50f6c2b98d4534537",
+     "q":"${from}_${to}",
+     "compact":"ultra"
+    });
+    http.Response res = await http.get(rateUrl);
+    if(res.statusCode ==200){
+       var body = jsonDecode(res.body);
+       return body["${from}_${to}"];
+    }else{
+      throw Exception("Failed to connect to API");
+    }
+  }
+
+
+
+ }
